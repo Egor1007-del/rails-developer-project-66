2fb76d5f7c7bd55f38274521307d8f2a20fb7be9
Egor1007-del <hyinm2012@yandex.ru>
2026-08-10 00:11:18 +0300

    added routes authorization omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get "home/index"
   root "home#index"
+  get "/auth/:provider/callback", to: "auth#callback"
+  get "/auth/failure", to: "auth#failure"
+
+  delete "/logout", to: "auth#logout", as: :logout
 end
