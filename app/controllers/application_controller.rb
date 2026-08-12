4ccad31de6185e55b75862b202a6e590eaa6b015
Egor1007-del <hyinm2012@yandex.ru>
2026-08-13 01:45:52 +0300

    user login check added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,10 @@ class ApplicationController < ActionController::Base
   def signed_in?
     current_user.present?
   end
+
+  def authenticate_user!
+    return if signed_in?
+
+    redirect_to root_path, alert: t("auth.required")
+  end
 end
