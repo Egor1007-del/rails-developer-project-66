7715a483485e60e4789d25fa42758c24cb34f656
Egor1007-del <hyinm2012@yandex.ru>
2026-08-13 00:21:23 +0300

    added relationships between the user and repositories models

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,9 @@
 class Repository < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, inverse_of: :repositories
+
+  validates :name, presence: true
+  validates :github_id, presence: true, uniqueness: { scope: :user_id }
+  validates :full_name, presence: true
+  validates :clone_url, presence: true
+  validates :ssh_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :repositories, dependent: :destroy, inverse_of: :user
+
   validates :provider, :uid, :email, :token, presence: true
   validates :email, uniqueness: true
   validates :uid, uniqueness: { scope: :provider }
