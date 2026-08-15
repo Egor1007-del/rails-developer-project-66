f5cdf47b12ab85e3a09c2865b55f3d44f7ada997
Egor1007-del <hyinm2012@yandex.ru>
2026-08-15 22:15:23 +0300

    add minitest-power_assert, update fixture for repositories and add model test repositories

--- test/models/repository_test.rb
+++ test/models/repository_test.rb
@@ -1,7 +1,77 @@
 require "test_helper"
 
 class RepositoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid repository" do
+    repository = repositories(:one)
+
+    assert { repository.valid? }
+  end
+
+  test "belongs to user" do
+    repository = repositories(:one)
+
+    assert { repository.user == users(:one) }
+  end
+
+  test "requires user" do
+    repository = repositories(:one)
+    repository.user = nil
+
+    assert { !repository.valid? }
+    assert { repository.errors[:user].any? }
+  end
+
+  test "requires attributes" do
+    attributes = %i[
+      name
+      github_id
+      full_name
+      clone_url
+      ssh_url
+    ]
+
+    attributes.each do |attribute|
+      repository = repositories(:one).dup
+      repository[attribute] = nil
+
+      assert { !repository.valid? }
+      assert { repository.errors[attribute].any? }
+    end
+  end
+
+  test "allows ruby language" do
+    repository = repositories(:one)
+    repository.language = :ruby
+
+    assert { repository.valid? }
+    assert { repository.language.ruby? }
+  end
+
+  test "does not allow unsupported language" do
+    repository = repositories(:one)
+    repository.language = :javascript
+
+    assert { !repository.valid? }
+    assert { repository.errors[:language].any? }
+  end
+
+  test "has ruby in supported languages" do
+    assert { Repository.language.values == [ "ruby" ] }
+  end
+
+  test "github id must be unique for user" do
+    repository = repositories(:one)
+
+    duplicate = repository.dup
+
+    assert { !duplicate.valid? }
+    assert { duplicate.errors[:github_id].any? }
+  end
+
+  test "different users can add same github repository" do
+    repository = repositories(:one).dup
+    repository.user = users(:two)
+
+    assert { repository.valid? }
+  end
 end
