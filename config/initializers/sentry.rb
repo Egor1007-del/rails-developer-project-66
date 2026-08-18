48f783baf0189efed4dce5745e4e6ccf97fa9921
Egor1007-del <hyinm2012@yandex.ru>
2026-08-18 23:07:51 +0300

    disconnecting Sentry from the test environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,5 +3,6 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [ :active_support_logger ]
   config.dsn = ENV["SENTRY_DSN"]
+  config.enabled_environments = %w[development production]
   config.traces_sample_rate = 1.0
 end
