2d4458c583a1cadc3eb81769ebcb8e5457b8e15f
Egor1007-del <hyinm2012@yandex.ru>
2026-08-19 23:08:53 +0300

    migrating the controllers to the web

---
+++ app/controllers/web/repositories/checks_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Web
+  module Repositories
+    class ChecksController < Web::ApplicationController
+      before_action :set_repository
+
+      def show
+        @check = @repository.checks.find(params[:id])
+      end
+
+      def create
+        @repository.checks.create!
+
+        redirect_to repository_path(@repository)
+      end
+
+      private
+
+      def set_repository
+        @repository =
+          current_user.repositories.find(params[:repository_id])
+      end
+    end
+  end
+end
