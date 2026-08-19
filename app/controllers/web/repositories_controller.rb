9ea4355bbe73d69576ac49fb6037f95c06491f67
Egor1007-del <hyinm2012@yandex.ru>
2026-08-19 22:46:58 +0300

    moving a Web Part to the Web namespace

---
+++ app/controllers/web/repositories_controller.rb
@@ -0,0 +1,44 @@
+module Web
+  class RepositoriesController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      @repositories = current_user.repositories
+    end
+
+    def new
+      @repository = Repository.new
+      @github_repositories = github_client.repositories
+    end
+
+    def show
+      set_repository
+    end
+
+    def create
+      @repository = BuildFromGithub.new.call(user: current_user, github_id: repository_params[:github_id])
+
+      if @repository.save
+        redirect_to repositories_path, notice: t(".success")
+      else
+        @github_repositories = github_client.repositories
+        flash.now[:alert] = t(".failure")
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def set_repository
+      @repository = current_user.repositories.find(params[:id])
+    end
+
+    def github_client
+      @github_client ||= ApplicationContainer[:github_client].new(current_user.token)
+    end
+
+    def repository_params
+      params.require(:repository).permit(:github_id)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root "home#index"
   get "/auth/:provider/callback", to: "auth#callback"
   get "/auth/failure", to: "auth#failure"
 
   delete "/logout", to: "auth#logout", as: :logout
+  scope module: :web do
+    root "home#index"
+    resources :repositories, only: [ :index, :show, :new, :create ]
+  end
 
-  resources :repositories, only: [ :index, :new, :create, :show ]
 
   if Rails.env.test?
     post "/test/session", to: "auth#test_session"
