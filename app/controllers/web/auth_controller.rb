b76ee0813459c11c8374914b64bb98e7c982a883
Egor1007-del <hyinm2012@yandex.ru>
2026-08-20 00:49:57 +0300

    migrate Auth in namespace the web

---
+++ app/controllers/web/auth_controller.rb
@@ -0,0 +1,54 @@
+module Web
+  class AuthController < ApplicationController
+    def callback
+      auth = request.env.fetch("omniauth.auth")
+
+      user = User.find_or_initialize_by(auth_params(auth))
+
+      user.nickname = auth.dig("info", "nickname")
+      user.name = auth.dig("info", "name")
+      user.email = auth.dig("info", "email")
+      user.image_url = auth.dig("info", "image")
+      user.token = auth.dig("credentials", "token")
+
+      user.save!
+
+      session[:user_id] = user.id
+
+      redirect_to root_path, notice: t(".success")
+    end
+
+    def failure
+      redirect_to root_path, alert: t(".failure")
+    end
+
+    def logout
+      reset_session
+      redirect_to root_path, notice: t(".success")
+    end
+
+    def test_session
+      email = params.require(:email)
+
+      user = User.find_or_create_by!(email: email) do |new_user|
+        new_user.provider = "test"
+        new_user.uid = email
+        new_user.nickname = email.split("@").first
+        new_user.token = "test_token"
+      end
+
+      session[:user_id] = user.id
+
+      head :ok
+    end
+
+    private
+
+    def auth_params(auth)
+      {
+        provider: auth["provider"],
+        uid: auth["uid"]
+      }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get "/auth/:provider/callback", to: "auth#callback"
-  get "/auth/failure", to: "auth#failure"
-
-  delete "/logout", to: "auth#logout", as: :logout
   scope module: :web do
+    get "/auth/:provider/callback", to: "auth#callback"
+    get "/auth/failure", to: "auth#failure"
+
+    delete "/logout", to: "auth#logout", as: :logout
+
     root "home#index"
+
     resources :repositories, only: [ :index, :show, :new, :create ] do
       resources :checks, only: [ :show, :create ], module: :repositories
     end
