7614c53e713d17f3ebde6b2c8261d3bed041cef5
Egor1007-del <hyinm2012@yandex.ru>
2026-08-21 22:50:06 +0300

    container registration new services

--- app/lib/application_container.rb
+++ app/lib/application_container.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 require Rails.root.join("test/stubs/github_client_stub") if Rails.env.test?
+require Rails.root.join("test/stubs/repository_loader_stub") if Rails.env.test?
+require Rails.root.join("test/stubs/rubocop_linter_stub") if Rails.env.test?
 
 class ApplicationContainer
   extend Dry::Container::Mixin
 
   if Rails.env.test?
     register(:github_client) { ::GithubClientStub }
+    register(:repository_loader) { ::RepositoryLoaderStub.new }
+    register(:linter) { ::RubocopLinterStub.new }
   else
     register(:github_client) { ::GithubClient }
+    register(:repository_loader) { ::RepositoryLoader.new }
+    register(:linter) { ::RubocopLinter.new }
   end
 end
