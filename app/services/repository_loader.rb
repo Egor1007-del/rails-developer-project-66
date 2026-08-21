75e113c95368e09f0a63e2c7b16795634785189b
Egor1007-del <hyinm2012@yandex.ru>
2026-08-21 22:21:58 +0300

    add REpositoryLoader service

---
+++ app/services/repository_loader.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "open3"
+require "tmpdir"
+require "fileutils"
+
+class RepositoryLoader
+  class Error < StandardError; end
+
+  def call(repository)
+    repository_path = Dir.mktmdir("repository-check-")
+
+    clone(repository.url, repository_path)
+
+    {
+      path: repository_path,
+      commit_id: commit_id(repository_path)
+    }
+  rescue
+    FileUtil.rm_rf(repository_path) if repository_path
+    raise
+  end
+
+  def cleanup(repository_path)
+    FileUtils.rm_rf(repository_path)
+  end
+
+  private
+
+  def clone(clone_url, repository_path)
+    _stdout, stderr, status = Open3.capture3(
+      "git",
+      "clone",
+      "--depth",
+      "1",
+      clone_url,
+      repository_path
+    )
+    raise Error, stderr unless status.success?
+  end
+
+  def commit_id(repository_path)
+    stdout, stderr, status = Open3.capture3(
+      "git",
+      "-C",
+      repository_path,
+      "rev-parse",
+      "HEAD"
+    )
+
+    raise Error, stderr unless status.success?
+
+    stdout.strip
+  end
+end
