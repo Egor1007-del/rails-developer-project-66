32daf66da2834a530b299941f7078fe0bb0d858c
Egor1007-del <hyinm2012@yandex.ru>
2026-08-22 02:33:34 +0300

    moved the Auth test to the 'web' namespace

--- test/integration/web/authentication_test.rb
+++ test/integration/web/authentication_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class AuthenticationTest < ActionDispatch::IntegrationTest
+class Web::AuthenticationTest < ActionDispatch::IntegrationTest
   test "guest sees sign in button only" do
     get root_path
 
