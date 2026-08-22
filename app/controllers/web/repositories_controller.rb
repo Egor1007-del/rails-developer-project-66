dc1eb0cfb8544d2d3fa97d41f4f1503c46310ae1
Egor1007-del <hyinm2012@yandex.ru>
2026-08-22 19:06:42 +0300

    adding service functionality

--- app/controllers/web/repositories_controller.rb
+++ app/controllers/web/repositories_controller.rb
@@ -8,7 +8,9 @@ module Web
 
     def new
       @repository = Repository.new
-      @github_repositories = github_client.repositories
+      @github_repositories = available_from_github.call(
+        user: current_user
+      )
     end
 
     def show
@@ -17,12 +19,14 @@ module Web
     end
 
     def create
-      @repository = BuildFromGithub.new.call(user: current_user, github_id: repository_params[:github_id])
+      @repository = build_from_github.call(user: current_user, github_id: repository_params[:github_id])
 
       if @repository.save
         redirect_to repositories_path, notice: t(".success")
       else
-        @github_repositories = github_client.repositories
+        @github_repositories = available_from_github.call(
+          user: current_user
+        )
         flash.now[:alert] = t(".failure")
         render :new, status: :unprocessable_entity
       end
@@ -30,12 +34,16 @@ module Web
 
     private
 
-    def set_repository
-      @repository = current_user.repositories.find(params[:id])
+    def build_from_github
+      @build_from_github ||= ApplicationContainer[:build_from_github]
     end
 
-    def github_client
-      @github_client ||= ApplicationContainer[:github_client].new(current_user.token)
+    def available_from_github
+      @available_from_github ||= ApplicationContainer[:available_from_github]
+    end
+
+    def set_repository
+      @repository = current_user.repositories.find(params[:id])
     end
 
     def repository_params
