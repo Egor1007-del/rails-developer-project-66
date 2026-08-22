6e435fde33a2cb94f16f55912f292e4ed7f13daf
Egor1007-del <hyinm2012@yandex.ru>
2026-08-22 21:51:47 +0300

    adding a working JSON

--- test/stubs/eslint_linter_stub.rb
+++ test/stubs/eslint_linter_stub.rb
@@ -1,13 +1,30 @@
 # frozen_string_literal: true
 
 class EslintLinterStub
-  OUTPUT = JSON.generate([])
+  OUTPUT = JSON.generate(
+    [
+      {
+        "filePath" => "/tmp/repository/app.js",
+        "messages" => [
+          {
+            "ruleId" => "no-undef",
+            "severity" => 2,
+            "message" => "'userName' is not defined.",
+            "line" => 2,
+            "column" => 13
+          }
+        ],
+        "errorCount" => 1,
+        "warningCount" => 0
+      }
+    ]
+  )
 
   def call(repository_path:)
     raise ArgumentError, "Repository path missing" if repository_path.blank?
 
     {
-      passed: true,
+      passed: false,
       output: OUTPUT
     }
   end
