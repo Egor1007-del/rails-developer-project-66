cd1a0c8bb8c4523815c7874ef053d2456b17ee7b
Egor1007-del <hyinm2012@yandex.ru>
2026-08-22 19:08:15 +0300

    adding shared methods for linters

--- app/models/repository/check.rb
+++ app/models/repository/check.rb
@@ -21,17 +21,83 @@ class Repository::Check < ApplicationRecord
     end
   end
 
+  def files_with_offeses
+    case repository.language.to_s
+    when "ruby"
+      rubocop_files
+    when "javascript"
+      eslint_files
+    else
+      []
+    end
+  end
+
+  def offense_count
+    case repository.language.to_s
+    when "ruby"
+        rubocop_offense_count
+    when "javascript"
+        eslint_offense_count
+    else
+        0
+    end
+  end
+
+  private
+
   def rubocop_files
-    parsed_output
+    parsed_result = parsed_output
+
+    return [] unless parsed_result.is_a?(Hash)
+
+    parsed_result
         .fetch("files", [])
         .select { |file| file.fetch("offenses", []).any? }
   end
 
-  def offense_count
-    parsed_output.dig("summary", "offense_count") || 0
+  def eslint_files
+    parsed_result = parsed_output
+
+    return [] unless parsed_result.is_a?(Array)
+
+    result.filter_map do |file|
+      messages = file.fetch("messages", [])
+
+      next if messages.empty?
+
+      {
+        "path" => file["filePath"],
+        "offenses" => messages.map do |message|
+          {
+            "message" => message["message"],
+            "cop_name" => message["ruleId"] || "ESLint",
+            "location" => {
+              "start_line" => message["line"],
+              "start_column" => message["column"]
+            }
+          }
+        end
+      }
+    end
   end
 
-  private
+  def rubocop_offense_count
+    result = parsed_output
+
+    return 0 unless result.is_a?(Hash)
+
+    result.dig("summary", "offense_count") || 0
+  end
+
+  def eslint_offense_count
+    result = parsed_output
+
+    return 0 unless result.is_a?(Array)
+
+    result.sum do |file|
+      file.fetch("messages", []).count
+    end
+  end
 
   def parsed_output
     return {} if output.blank?
