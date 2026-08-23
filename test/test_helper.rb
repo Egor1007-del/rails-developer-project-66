ccf1c3bd67dcbb217eb0c9db944428094ebadafe
Egor1007-del <hyinm2012@yandex.ru>
2026-08-23 18:34:26 +0300

    test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,7 +16,5 @@ module ActiveSupport
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
