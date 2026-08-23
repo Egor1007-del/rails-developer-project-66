2146955aa570694be99492572f6f8ca7f3c683fa
Egor1007-del <hyinm2012@yandex.ru>
2026-08-23 20:53:45 +0300

    added repo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV.fetch("GITHUB_CLIENT_ID", "mock_id"), ENV.fetch("GITHUB_CLIENT_SECRET", "mock_secret"), scope: "user:email"
+  provider :github, ENV.fetch("GITHUB_CLIENT_ID", "mock_id"), ENV.fetch("GITHUB_CLIENT_SECRET", "mock_secret"), scope: "user:email,repo"
 end
