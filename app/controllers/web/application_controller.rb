62502d9d94c2a80a88fd135f07f7a29f7e8f8060
Egor1007-del <hyinm2012@yandex.ru>
2026-08-25 23:22:52 +0300

    moving private methods

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -2,5 +2,22 @@
 
 module Web
   class ApplicationController < ::ApplicationController
+    helper_method :current_user, :signed_in?
+
+    private
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def signed_in?
+      current_user.present?
+    end
+
+    def authenticate_user!
+      return if signed_in?
+
+      redirect_to root_path, alert: t("auth.required")
+    end
   end
 end
