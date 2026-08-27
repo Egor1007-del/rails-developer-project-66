84b22fec2214217c036e830ff4885ef7a0bc499a
Egor1007-del <hyinm2012@yandex.ru>
2026-08-28 01:24:55 +0300

    adding a GitHub API request and caching the selected language

--- app/services/repositories/available_from_github.rb
+++ app/services/repositories/available_from_github.rb
@@ -3,16 +3,20 @@ module Repositories
     include Import["github_client"]
 
     def call(user:)
-      github_repositories = github_client
-        .new(user.token)
-        .repositories
+      Rails.cache.fetch(
+        [ "available-github-repositories", user.cache_key_with_version ],
+        expres_in: 5.minutes) do
+        github_repositories = github_client
+          .new(user.token)
+          .repositories
 
-      supported_languages = Repository.language.values.map(&:to_s)
+        supported_languages = Repository.language.values.map(&:to_s)
 
-      github_repositories.select do |github_repository|
-        language = github_repository.language&.downcase
+        github_repositories.select do |github_repository|
+          language = github_repository.language&.downcase
 
-        supported_languages.include?(language)
+          supported_languages.include?(language)
+        end
       end
     end
   end
