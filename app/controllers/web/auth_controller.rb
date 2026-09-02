8d74d96815971676e5e1a9a3af8714e3e08cb342
Egor1007-del <hyinm2012@yandex.ru>
2026-09-02 22:16:44 +0300

    deletion of the test route

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -27,21 +27,6 @@ module Web
       redirect_to root_path, notice: t(".success")
     end
 
-    def test_session
-      email = params.require(:email)
-
-      user = User.find_or_create_by!(email: email) do |new_user|
-        new_user.provider = "test"
-        new_user.uid = email
-        new_user.nickname = email.split("@").first
-        new_user.token = "test_token"
-      end
-
-      sign_in(user)
-
-      head :ok
-    end
-
     private
 
     def auth_params(auth)
