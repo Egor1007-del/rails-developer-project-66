8a0e24b9f89afd9a6a5a583abb7955817192b297
Egor1007-del <hyinm2012@yandex.ru>
2026-09-04 02:09:46 +0300

    updated message templates and tests

--- test/mailers/repository_check_mailer_test.rb
+++ test/mailers/repository_check_mailer_test.rb
@@ -3,18 +3,44 @@ require "test_helper"
 class RepositoryCheckMailerTest < ActionMailer::TestCase
   setup do
     @repository = repositories(:one)
-    @check = @repository.checks.create!(
+  end
+
+  test "error check email" do
+    check = @repository.checks.create!(
+      aasm_state: "failed",
+      passed: nil
+    )
+
+    email = RepositoryCheckMailer.with(
+      user: @repository.user,
+      check: check
+    ).error_check_email
+
+    assert_equal [ @repository.user.email ], email.to
+
+    assert_equal I18n.t(
+      "repository_check_mailer.error_check_email.subject"
+    ), email.subject
+
+    assert_match @repository.full_name, email.text_part.body.to_s
+    assert_match "http", email.text_part.body.to_s
+  end
+
+  test "failed check email" do
+    check = @repository.checks.create!(
       aasm_state: "finished",
       passed: false
     )
-  end
 
-  test "check failed email" do
-    email = RepositoryCheckMailer.check_failed(@check)
+    email = RepositoryCheckMailer.with(
+      user: @repository.user,
+      check: check
+    ).failed_check_email
 
     assert_equal [ @repository.user.email ], email.to
+
     assert_equal I18n.t(
-      "repository_check_mailer.check_failed.subject"
+      "repository_check_mailer.failed_check_email.subject"
     ), email.subject
 
     assert_match @repository.full_name, email.text_part.body.to_s
