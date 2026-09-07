15d9d1b45d6c8be528eebd4d5b9a928f05482c40
Egor1007-del <hyinm2012@yandex.ru>
2026-09-08 00:46:25 +0300

    return to the link

--- test/integration/web/repositories_test.rb
+++ test/integration/web/repositories_test.rb
@@ -136,10 +136,8 @@ class Web::RepositoriesTest < ActionDispatch::IntegrationTest
     assert_select "h1",
                   text: repository.full_name
 
-    assert_select "form[action='#{repository_checks_path(repository)}'][method='post']" do
-      assert_select "button",
+    assert_select "a[href='#{repository_checks_path(repository)}']",
                     text: I18n.t("web.repositories.show.check")
-    end
   end
 
   test "user cannot view another users repository" do
