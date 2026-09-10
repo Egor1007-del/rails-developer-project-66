b4da6a273d230d2304978cc0553752f71ecb8c4d
Egor1007-del <hyinm2012@yandex.ru>
2026-09-11 01:24:16 +0300

    the call for prepared data has been updated

--- app/lib/stubs/github_client_stub.rb
+++ app/lib/stubs/github_client_stub.rb
@@ -10,47 +10,19 @@ module Stubs
       end
     end
 
-    GithubRepository = Struct.new(
-      :id,
-      :name,
-      :full_name,
-      :language,
-      :clone_url,
-      :ssh_url,
-      keyword_init: true
-    )
-
-    RUBY_REPOSITORY = GithubRepository.new(
-      id: 10_001,
-      name: "github-rails-project",
-      full_name: "user-one/github-rails-project",
-      language: "Ruby",
-      clone_url: "https://github.com/user-one/github-rails-project.git",
-      ssh_url: "git@github.com:user-one/github-rails-project.git"
-    )
-
-    JAVASCRIPT_REPOSITORY = GithubRepository.new(
-      id: 20_001,
-      name: "javascript-project",
-      full_name: "user-one/javascript-project",
-      language: "JavaScript",
-      clone_url: "https://github.com/user-one/javascript-project.git",
-      ssh_url: "git@github.com:user-one/javascript-project.git"
-    )
-
-    PYTHON_REPOSITORY = GithubRepository.new(
-      id: 30_001,
-      name: "python-project",
-      full_name: "user-one/python-project",
-      language: "Python",
-      clone_url: "https://github.com/user-one/python-project.git",
-      ssh_url: "git@github.com:user-one/python-project.git"
-    )
-
     def initialize(*); end
 
     def repositories
-      [ RUBY_REPOSITORY, JAVASCRIPT_REPOSITORY, PYTHON_REPOSITORY ]
+      data = JSON.parse(
+        Rails.root.join(
+          "test/fixtures/files/user_repositories.json"
+        ).read,
+        symbolize_names: true
+      )
+
+      data.map do |params|
+        GithubRepositoryStub.new(params)
+      end
     end
 
     def repository(github_id)
